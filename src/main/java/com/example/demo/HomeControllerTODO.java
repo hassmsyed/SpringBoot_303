635f0007f5db1cb962622dea459581ba8c541aef
hassmsyed <hassmsyed@gmail.com>
2019-08-27 16:12:59 -0400

    SpringBoot_303

---
+++ src/main/java/com/example/demo/HomeControllerTODO.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeControllerTODO {
+    @Autowired
+    TodoRepository todoRepository;
+
+    @RequestMapping("/")
+    public String listTask(Model model){
+        model.addAttribute("tasks", todoRepository.findAll());
+        return "tasklist";
+    }
+    @GetMapping("/add")
+    public String courseForm(Model model){
+        model.addAttribute("task", new Todo());
+        return "taskform";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid Todo task, BindingResult result ){
+        if (result.hasErrors()){
+            return "taskform";
+        }
+        todoRepository.save(task);
+        return "redirect:/";
+    }
+    @RequestMapping("/detail/{id}")
+    public String showTask(@PathVariable("id") long id, Model model)
+    {
+        model.addAttribute("task", todoRepository.findById(id).get());
+        return "show";
+    }
+    @RequestMapping("/update/{id}")
+    public String updateTask(@PathVariable("id") long id, Model model){
+        model.addAttribute("task", todoRepository.findById(id).get());
+        return "taskform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String delTask(@PathVariable("id") long id){
+        todoRepository.deleteById(id);
+        return "redirect:/";
+
+    }
+}
